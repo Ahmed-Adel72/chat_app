3fab39b604d00dc507bbec011f6331ce8db71dc7
DESKTOP-GIGKCG7\ahmed <ahmedadel121216@gmail.com>
2024-10-01 02:10:19 +0300

    - added splash screen

---
+++ lib/chat_app.dart
@@ -0,0 +1,18 @@
+import 'package:chat_app/features/auth/views/login_view.dart';
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
